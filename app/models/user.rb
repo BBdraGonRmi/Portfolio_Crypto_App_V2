9f67c2f623268d4f4bcf8b6afa59745d3b309154
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-06-19 15:39:29 +0200

    🦺 add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  before_save { self.email.downcase! }
+
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: {minimum: 3, maximum: 25}
+  VALID_EMAIL_REGEX = /.+\@.+\..+/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
