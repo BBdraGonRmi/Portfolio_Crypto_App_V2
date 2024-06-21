2ced16c9d945f1172f299de54a5f5f9d5e27dbc6
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-06-21 17:13:23 +0200

    🤡 🍱 ✨ create session

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'pages#home'
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
