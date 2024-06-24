27d64cf9fa639197bc06ba18380a1bcd980b589e
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-06-24 17:03:07 +0200

    🦺 add validations in transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,30 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :wallet
+
+  validates :user_id, presence: true
+  validates :wallet_id, presence: true
+
+  validates :datetime_of_transaction, presence: true
+
+  validates :operation, presence: true,
+                        length: { minimum: 2, maximum: 10 }
+
+  validates :symbol, presence: true,
+                    length: { minimum: 2, maximum: 10 }
+
+  validates :amount, presence: true,
+                    numericality: true
+
+  validates :price, presence: true,
+                    numericality: true
+
+  validates :net_value, presence: true,
+                        numericality: true
+
+  validates :fees, presence: true,
+                        numericality: true
+
+  validates :total_value, presence: true,
+                          numericality: true
 end
