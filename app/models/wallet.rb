f3ad99f1d1823f539f783b397b34094c01918336
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-06-24 15:24:22 +0200

    🦺 add validations in wallet model

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,7 @@
 class Wallet < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :name, presence: true,
+                    length: { minimum: 2, maximum: 20 }
 end
