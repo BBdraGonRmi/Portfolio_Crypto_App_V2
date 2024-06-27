a0297e9179fecb33c9738cab94d6c977a97960b2
BBdraGonRmi <dupouy.jehan@gmail.com>
2024-06-27 18:32:03 +0200

    💄 add portfolio distribution pie chart on dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,61 +4,6 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @bitcoin_data = {
-      "2023-06-01" => 1000,
-      "2023-07-01" => 1500,
-      "2023-08-01" => 1200,
-      "2023-09-01" => 1700,
-      "2023-10-01" => 1400,
-      "2023-11-01" => 1800
-    }
-
-    @ethereum_data = {
-      "2020-12-01" => 900,
-      "2021-01-01" => 1300,
-      "2021-02-01" => 1100,
-      "2021-03-01" => 1600
-    }
-
-    @deposits = Transaction.where(user_id: current_user.id, operation: "DEPOSIT").order(:datetime_of_transaction)
-    @deposits_data = {}
-    net_value = 0
-
-    @deposits.each do |transaction|
-      Rails.logger.info "DEPOSITS: #{transaction.net_value}"
-      net_value += transaction.net_value
-      #Rails.logger.info "NET VALUE: #{net_value}"
-      date = transaction.datetime_of_transaction.to_date
-      @deposits_data[date] = net_value
-    end
-
-    #fill_missing_months_with_last_value(@deposits, @deposits_data)
-
-    @monthly_deposits_data = {}
-    @deposits_data.each do |date, net_value|
-      month_start = date.beginning_of_month
-      @monthly_deposits_data[month_start] = net_value
-    end
-
-    @transactions = Transaction.where(user_id: current_user.id).order(:datetime_of_transaction)
-    @transactions_data = {}
-    net_value = 0
-
-    @transactions.each do |transaction|
-      net_value += transaction.net_value
-      #Rails.logger.info "NET VALUE: #{net_value}"
-      date = transaction.datetime_of_transaction.to_date
-      @transactions_data[date] = net_value
-    end
-
-    #fill_missing_months_with_last_value(@transactions, @transactions_data)
-
-    @monthly_transactions_data = {}
-    @transactions_data.each do |date, net_value|
-      month_start = date.beginning_of_month
-      @monthly_transactions_data[month_start] = net_value
-    end
-
 
     @total_balance = 0
     @symbols = Transaction.where(user_id: current_user.id).select(:symbol).distinct.pluck(:symbol)
@@ -92,10 +37,16 @@ class PagesController < ApplicationController
       }
     end
 
+    @tokens_data.delete_if { |token| token[:balance_in_dollars] == nil || token[:balance_in_dollars] <= 0 }
+    @tokens_data.sort_by! { |token| token[:balance_in_dollars] }.reverse!
+
+    @portfolio_distribution = {}
+
     @tokens_data.each do |token|
 
       if Float(token[:current_price], exception: false)
         token[:portfolio_percentage] = ((token[:balance] * token[:current_price]) / @total_balance) * 100
+        @portfolio_distribution[token[:symbol]] = token[:portfolio_percentage]
       else
         token[:portfolio_percentage] = nil
       end
